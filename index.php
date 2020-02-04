23061fcca6169b3a4e1ad4fbb7b88a83e415a41b
Arnold Csisztai <csisztai.arnold@gmail.com>
2020-02-04 07:50:42 +0100

    Search by keyword added.

--- index.php
+++ index.php
@@ -8,14 +8,22 @@ $search = [
     'type' => 'video',
     'order' => 'date',
     'maxResults' => 50,
-    'location' => '46.2695492,20.1335611',
-    'locationRadius' => '10km',
+];
+
+$search_by_keyword = [
+    'q' => 'szeged',
+];
+
+$search_by_location = [
+    'location' => '46.2695492,20.1335611', // Szeged
+    'locationRadius' => '30km',
 ];
 
 $youtube = new Madcoda\Youtube(array('key' => $settings['YOUTUBE_API_KEY']));
-$videos = $youtube->searchAdvanced($search);
+$videos_by_keywords = $youtube->searchAdvanced(array_merge($search, $search_by_keyword));
+$videos_by_location = $youtube->searchAdvanced(array_merge($search, $search_by_location));
 
-foreach ($videos as $video) {
+foreach (array_merge($videos_by_keywords, $videos_by_location) as $video) {
     echo '<h1>' . $video->snippet->title . '</h1>';
     echo '<h2>' . $video->snippet->publishedAt . '</h2>';
     echo '<a href="https://www.youtube.com/watch?v=' . $video->id->videoId . '"><img src="' . $video->snippet->thumbnails->high->url.'" ></a><hr />';
